6de442896f5aa295bbdcf2c7b0d8110689ee1189
Gabriel Moreira Mourão Dai <Gabriel.Dai@notacontrol.matriz>
2022-07-27 17:11:50 -0400

    movimentos possiveis

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var dimensao = 8;
 // CÓDIGO UNICODE DA PEÇA E POSIÇÕES INICIAIS
 // TORRE, CAVALO, BISPO, REI, RAINHA, PEÃO
 var brancas = [
-    ['&#9814', '1A', '1H'],
-    ['&#9816', '1B', '1G'],
-    ['&#9815', '1C', '1F'],
-    ['&#9812', '1D'],
-    ['&#9813', '1E'],
-    ['&#9817', '2*']
+    ['&#9814', '1A', '1H', 'T'],
+    ['&#9816', '1B', '1G', 'C'],
+    ['&#9815', '1C', '1F', 'B'],
+    ['&#9812', '1D', 'r'],
+    ['&#9813', '1E', 'R'],
+    ['&#9817', '2*', 'P']
 ];
 
 var pretas = [
-    ['&#9820', '8A', '8H'],
-    ['&#9822', '8B', '8G'],
-    ['&#9821', '8C', '8F'],
-    ['&#9818', '8D'],
-    ['&#9819', '8E'],
-    ['&#9823', '7*']
+    ['&#9820', '8A', '8H', 'T'],
+    ['&#9822', '8B', '8G', 'C'],
+    ['&#9821', '8C', '8F', 'B'],
+    ['&#9818', '8D', 'r'],
+    ['&#9819', '8E', 'R'],
+    ['&#9823', '7*', 'P']
 ];
 
 var cores = [brancas, pretas];
@@ -41,21 +41,22 @@ function criaTabuleiro() {
 }
 
 function populaTabuleiro() {
-    for (let j = 0; j < cores.length; j++) {
-        let corAtual = cores[j];
-        for (let i = 0; i < corAtual.length; i++) {
-            if (corAtual[i].length == 3) {
-                let ids = [corAtual[i][1], corAtual[i][2]];
+    cores.forEach(cor => {
+        cor.forEach(e => {
+            if (e.length == 4) {
+                let ids = [e[1], e[2]];
                 let celulas = [document.getElementById(ids[0]), document.getElementById(ids[1])];
                 
                 let pecas = [document.createElement('div'), document.createElement('div')];
                 pecas[0].className = 'peca';
                 pecas[1].className = 'peca';
-                pecas[0].innerHTML = `<p>${corAtual[i][0]}</p>`;
-                pecas[1].innerHTML = `<p>${corAtual[i][0]}</p>`;
+                pecas[0].id = e[3];
+                pecas[1].id = e[3];
+                pecas[0].innerHTML = `<p>${e[0]}</p>`;
+                pecas[1].innerHTML = `<p>${e[0]}</p>`;
                 
-                pecas[0].setAttribute('onclick', 'teste()');
-                pecas[1].setAttribute('onclick', 'teste()');
+                pecas[0].setAttribute('onclick', 'mostraMovimentos(this)');
+                pecas[1].setAttribute('onclick', 'mostraMovimentos(this)');
                 
                 celulas[0].appendChild(pecas[0]);
                 celulas[1].appendChild(pecas[1]);
@@ -63,16 +64,17 @@ function populaTabuleiro() {
                 pecasJogo.push(pecas[0]);
                 pecasJogo.push(pecas[1]);
             }
-            else if (corAtual[i].length == 2) {
-                let id = corAtual[i][1];
+            else if (e.length == 3) {
+                let id = e[1];
                 if (id[1] != '*') {
 
                     let celula = document.getElementById(id);
                     
                     let peca = document.createElement('div');
                     peca.className = 'peca';
-                    peca.innerHTML = `<p>${corAtual[i][0]}</p>`;
-                    peca.setAttribute('onclick', 'teste()');
+                    peca.id = e[2];
+                    peca.innerHTML = `<p>${e[0]}</p>`;
+                    peca.setAttribute('onclick', 'mostraMovimentos(this)');
                     celula.appendChild(peca);
 
                     pecasJogo.push(peca);
@@ -84,18 +86,25 @@ function populaTabuleiro() {
                         
                         let peca = document.createElement('div');
                         peca.className = 'peca';
-                        peca.innerHTML = `<p>${corAtual[i][0]}</p>`;
-                        peca.setAttribute('onclick', 'teste()');
+                        peca.id = e[2];
+                        peca.innerHTML = `<p>${e[0]}</p>`;
+                        peca.setAttribute('onclick', 'mostraMovimentos(this)');
                         celula.appendChild(peca);
 
                         pecasJogo.push(peca);
                     }
                 }
             }
-        }
-    }
+        })
+    })
 }
 
-function teste() {
-    alert('clicou');
+function mostraMovimentos(peca) {
+    let id = peca.parentNode.id.split('');
+    console.log(id);
+    if (peca.id = 'C') {
+        let divId = `${parseInt(id[0])+2}${String.fromCharCode(id[1].charCodeAt(0)+1)}`
+        let div = document.getElementById(divId);
+        div.style.background = 'orange';
+    }
 }
